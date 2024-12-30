c3556030c85b0694983c2693525ec1867e990385
Thomas Moore <tcm0116@gmail.com>
2024-12-30 18:44:40 -0500

    Run LED processing at 25 Hz

--- LCM/Code/User/main.c
+++ LCM/Code/User/main.c
@@ -70,7 +70,7 @@ int main(void)
 	{
 		KEY1_Task();
 		
-		if(WS2812_Counter >= 20) // 20ms refresh period
+		if(WS2812_Counter >= 50) // 50ms refresh period
 		{
 			WS2812_Task();
 			WS2812_Counter = 0;
