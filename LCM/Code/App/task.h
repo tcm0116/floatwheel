1a3a5a4ba6616da22c5a56073c553505c90f2ae3
Thomas Moore <tcm0116@gmail.com>
2024-12-30 21:57:07 -0500

    Increase VESC boot time

--- LCM/Code/App/task.h
+++ LCM/Code/App/task.h
@@ -37,7 +37,7 @@
 
 #define   SHUTDOWN_TIME		  		20
 #define   VESC_RPM            		1000
-#define   VESC_BOOT_TIME      		4000
+#define   VESC_BOOT_TIME      		5000
 #define   VESC_SHUTDOWN_TIME      	1000
 #define   DUTY_CYCLE          		0.9
 #define   VOLTAGE_RECEIPT     		0.02
